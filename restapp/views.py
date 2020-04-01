eba9e7e2d0099b888dbf32fb84fef67890029bc7
rshalem <shalem000@gmail.com>
2020-04-01 15:56:43 +0530

    REST Framework

--- restapp/views.py
+++ restapp/views.py
@@ -1,11 +1,15 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework import status
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Question
 from .serializers import QuestionSerializer
 
 # Create your views here.
-
+# without wrapper for API view
 @csrf_exempt
 def questions_list(request):
 
@@ -23,6 +27,7 @@ def questions_list(request):
     elif request.method == "POST":
         # parsing post data from user to JSON
         data = JSONParser().parse(request)
+        # for creation of new object we have to pass the data as data=data
         serializer = QuestionSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
@@ -31,31 +36,36 @@ def questions_list(request):
             return JsonResponse(serializer.errors, status=400)
 
 # getting , updating & deleting a single instance
+# wrapping API views @api view decorator for function based views
+# Response comprises of all responses like HTTP or JSON response
+
+# WITH @API VIEW DECORATOR (Wrapper)
 @csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def question_detail(request, id):
 
     try:
         single_queryset = Question.objects.get(pk=id)
     except Question.DoesNotExist:
-        return JsonResponse({"error": "Instance not found"}, status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     # GET for single instance
     if request.method == "GET":
         serializer = QuestionSerializer(single_queryset)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
+        data = request.data
         # takes in instance as a para by which it knows to edit an existing object & not to create new one
         serializer = QuestionSerializer(instance=single_queryset, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         single_queryset.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
